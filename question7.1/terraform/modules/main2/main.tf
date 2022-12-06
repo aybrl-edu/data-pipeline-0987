ac241a9f5f1cf73f44113c0421905d204f04d0b2
Aybrl <contact@aybrl.com>
2022-12-06 17:44:58 +0100

    question7.1

--- question7.1/terraform/modules/main2/main.tf
+++ question7.1/terraform/modules/main2/main.tf
@@ -16,4 +16,9 @@ resource "aws_kinesis_stream" "ayoub-berhili-stock-input-stream" {
     Environment = "test"
     Etudiant = "mohammed-ayoub.berhili@etu.u-pec.fr"
   }
+
+  # Defines IAM roles for the ECS instance 
+  iam_instance_profile = aws_iam_instance_profile.ec2_profile.name
+  
+  vpc_security_group_ids = ["${aws_security_group.default.id}"] 
 }
